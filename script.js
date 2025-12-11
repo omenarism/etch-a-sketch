440544b626c1590297c14b0187cc6a20f9abf3d2
Uthman <68771485+omenarism@users.noreply.github.com>
2025-12-11 17:15:56 +0800

    completed the tasks w/o the extra credit

--- script.js
+++ script.js
@@ -4,17 +4,7 @@ square.style.width = "800px";
 square.style.height = "800px";
 document.body.appendChild(square);
 
-const grid = document.createElement("div");
-grid.className = "grid";
-grid.style.width = "50px";
-grid.style.height = "50px";
-grid.style.border = "1px solid #000";
-
-let input = +prompt("Enter a number below 100");
-let rows = input;
-let cols = input;
-
-function drawGrid() {
+function drawGrid(rows, cols) {
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       const gridCell = document.createElement("div");
@@ -22,10 +12,27 @@ function drawGrid() {
       gridCell.style.height = `${800 / rows}px`;
       gridCell.classList.add("hover");
       square.appendChild(gridCell);
-      gridCell.addEventListener("click", () => {
+      gridCell.addEventListener("mouseover", () => {
         gridCell.style.backgroundColor = "#000";
       });
     }
   }
 }
-drawGrid();
+
+function getValueLessThanFifty() {
+  let input;
+  while (true) {
+    input = +prompt("Enter a number below 50");
+    if (input <= 50) {
+      return input;
+    }
+  }
+}
+
+drawGrid(16, 16);
+
+document.querySelector("#btn-reset").addEventListener("click", () => {
+  document.querySelectorAll(".hover").forEach((e) => e.remove()); //savior from strangers
+  let ExAndWhy = getValueLessThanFifty();
+  drawGrid(ExAndWhy, ExAndWhy);
+});
